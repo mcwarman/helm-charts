365cbe124e28b4b1750aefb58ebf21b8aaa1d15e
Matthew Warman <mcwarman@gmail.com>
2024-01-19 09:16:51 +0000

    feat(ci): add link to updatecli changelog (#106)

--- updatecli/update-changelog.sh
+++ updatecli/update-changelog.sh
@@ -3,16 +3,12 @@
 set -euo pipefail
 
 dir=$1
-message_format=$2
-version=$3
+log_line=$2
 
 git checkout main -- "${dir}/CHANGELOG.md"
 
 chart_version=$(yq '.version' "${dir}/Chart.yaml")
 date=$(date "+%Y-%m-%d")
-
-# shellcheck disable=SC2059
-log_line=$(printf "$message_format" "$version")
 
 log="\n## ${chart_version} - ${date}\n\n### Changed\n\n- ${log_line}"
 
